012518ffcc8d67ab0efe504f73f1143998a38cd1
FraterSKS <jackson.a.furr@gmail.com>
2022-10-23 21:56:37 -0400

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+//VIEWS
+import Main from "./views/Main";
+import Navbar from "./layouts/Navbar";
+import Footer from "./layouts/Footer";
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <Main />
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/layouts/Navbar.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+const Navbar: React.FC = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top mb-3">
+      <div className="container-fluid">
+        <div className="navbar-brand" id="nav-title">Belay</div>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavDropdown"
+          aria-controls="navbarNavDropdown"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <div className="nav-link">Home</div>
+            </li>
+          </ul>
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <div className="nav-link">
+                Login
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
