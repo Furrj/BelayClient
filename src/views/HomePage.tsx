935a78323d75f67af16dc8f809bfa222ebf3ee4d
FraterSKS <jackson.a.furr@gmail.com>
2022-11-29 14:37:00 -0500

    removed cookies

--- src/views/HomePage.tsx
+++ src/views/HomePage.tsx
@@ -1,20 +1,11 @@
 import React, { useState } from "react";
-import Cookies from "js-cookie";
 
 const HomePage: React.FC = () => {
   const [userId, setUserId] = useState<string>("");
 
-  const readCookie = () => {
-    const userID: any = Cookies.get("user_id");
-    console.log(userId);
-    const newUserID = userId.substring(3, 20);
-    console.log(newUserID);
-  };
-
   return (
     <div>
       <h1>Home Page</h1>
-      <button onClick={readCookie}>Cookie</button>
     </div>
   );
 };
