060f33722487ff4da5ab0f94ae3f96ecf4eefb9b
FraterSKS <jackson.a.furr@gmail.com>
2022-11-28 19:22:05 -0500

    basic cookie

--- src/views/HomePage.tsx
+++ src/views/HomePage.tsx
@@ -4,13 +4,14 @@ import Cookies from "js-cookie";
 const HomePage: React.FC = () => {
   const [userId, setUserId] = useState<string>("");
 
-  if (Cookies.get('user_id')) {
-    console.log("User Id:" + Cookies.get('user_id'));
+  const readCookie = () => {
+    console.log(Cookies.get("user_id"));
   }
 
   return (
     <div>
       <h1>Home Page</h1>
+      <button onClick={readCookie}>Cookie</button>
     </div>
   );
 };
