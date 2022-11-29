abd3fa4f58ecfe4cc81abec3d573946477f63c40
FraterSKS <jackson.a.furr@gmail.com>
2022-11-28 21:47:37 -0500

    trying auth

--- src/views/HomePage.tsx
+++ src/views/HomePage.tsx
@@ -1,12 +1,15 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Cookies from "js-cookie";
 
 const HomePage: React.FC = () => {
   const [userId, setUserId] = useState<string>("");
 
   const readCookie = () => {
-    console.log(Cookies.get("user_id"));
-  }
+    const userID: any = Cookies.get("user_id");
+    console.log(userId);
+    const newUserID = userId.substring(3, 20);
+    console.log(newUserID);
+  };
 
   return (
     <div>
